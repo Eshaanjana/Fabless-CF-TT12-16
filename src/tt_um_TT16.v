9491afdedb5387b56b1d1d7ca60eae7e1ce94981
Eshaanjana S <eshapriya2006@gmail.com>
2025-08-26 22:51:09 +0530

    Update tt_um_TT16.v

--- src/tt_um_TT16.v
+++ src/tt_um_TT16.v
@@ -5,6 +5,9 @@
 
 `default_nettype none
 
+parameter DATA_WIDTH = 4;
+parameter ADDR_WIDTH = 3;
+
 module tt_um_TT16 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
@@ -18,10 +21,17 @@ module tt_um_TT16 (
 
   // All output pins must be assigned. If not used, assign to 0.
     // Example: ou_out is the sum of ui_in and uio_in
+   wire wclk,rclk;
+
+   wire [ADDR_WIDTH:0] wptr, rptr;
+   wire [ADDR_WIDTH-1:0] waddr, raddr;
+   wire [ADDR_WIDTH:0] rptr_sync, wptr_sync;
+    
    wire winc = ui_in[4];
    wire rinc = ui_in[5];
    reg [3:0] wdata;
    wire [3:0] rdata;
+   wire full,empty;
     
   always @(*) begin
       wdata[0] = ui_in[0];
@@ -30,18 +40,43 @@ module tt_um_TT16 (
       wdata[3] = ui_in[3];
 end
 
-    assign uo_out[3:0] =  rdata[3:0];
-    assign uo_out[4] = full;
-    assign uo_out[5] = empty;
+    assign uo_out = {2'b00, empty, full, rdata[3:0]};
     
     assign uio_out = 0;
     assign uio_oe  = 0;
+
+   clock_div clk_div_inst (
+        .clk(clk),
+        .rst_n(rst_n),
+        .wclk(wclk),
+        .rclk(rclk)
+    );
+    
+    fifo_memory #(DATA_WIDTH, ADDR_WIDTH) mem (
+        .wclk(wclk), .rclk(rclk), .waddr(waddr), .raddr(raddr),
+        .wdata(wdata), .wen(winc & ~full), .ren(rinc & ~empty), .rdata(rdata)
+    );
+
+    wptr_full #(ADDR_WIDTH) wptr_inst (
+        .wclk(wclk), .rst_n(rst_n), .winc(winc),
+        .rptr_sync(rptr_sync), .full(full),
+        .wptr(wptr), .waddr(waddr)
+    );
+
+    rptr_empty #(ADDR_WIDTH) rptr_inst (
+        .rclk(rclk), .rst_n(rst_n), .rinc(rinc),
+        .wptr_sync(wptr_sync), .empty(empty),
+        .rptr(rptr), .raddr(raddr)
+    );
+
+    two_ff_sync #(ADDR_WIDTH) sync_r2w (
+        .clk(wclk), .rst_n(rst_n), .d(rptr), .q(rptr_sync)
+    );
+
+    two_ff_sync #(ADDR_WIDTH) sync_w2r (
+        .clk(rclk), .rst_n(rst_n), .d(wptr), .q(wptr_sync)
+    );
     
   // List all unused inputs to prevent warnings
-    wire _unused = &{ena, ui_in[6],ui_in[7],uo_out[6],uo_out[7],uio_in[7:0]};
-    
-    fifo #(DATA_WIDTH, ADDR_WIDTH) fifo_inst(
-        .winc(winc), .rinc(rinc), .wclk(wclk), .rclk(rclk), .clk(clk), .rst_n(rst_n), 
-        .wdata(wdata), .rdata(rdata), .full(full), .empty(empty)
-    );
+    wire _unused = &{ena, ui_in[6],ui_in[7],uio_in[7:0]};
 endmodule
